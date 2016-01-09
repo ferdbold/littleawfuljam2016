5205e66733ba433f2f24288d657f43e17f92fd8a
Antoine Brouillette <antoineqc@hotmail.com>
2016-01-09 15:48:50 -0500

    Lever and Computer Hacking logic completed

---
+++ Assets/Scripts/EnviroElement/Lever.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class Lever : Interactable {
+
+    [SerializeField]
+    private UnityEvent relatedEvent;
+
+    private bool activated = false;
+
+    public override void Activate() {
+        if (!activated) {
+            relatedEvent.Invoke();
+            //TODO Start Anim
+        }
+    }
+}
