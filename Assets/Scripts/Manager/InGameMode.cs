00feb43ec9c52aed76d8e61b32d1551508457f9b
Antoine Brouillette <antoineqc@hotmail.com>
2016-01-08 21:19:41 -0500

    Basic Gameloop and InGame Manager

---
+++ Assets/Scripts/Manager/InGameMode.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameMode : MonoBehaviour {
+
+    public static InGameMode instance;
+
+    void Start() {
+        if (instance == null) {
+            instance = this;
+            currentState = GameState.playMode;
+        }
+        else {
+            Destroy(this);
+        }
+    }
+
+    void OnDestroy() {
+        if (instance == this) {
+            instance = null;
+        }
+    }
+
+    //-------------------------------------------------------------------------------------------------------------------------
+
+    public enum GameState { playMode, killMode }
+    public GameState currentState { get; private set; }
+
+    public void Activate_KillMode() {
+        SwitchState(GameState.killMode);
+    }
+
+    public void Activate_PlayMode() {
+        SwitchState(GameState.playMode);
+    }
+
+    private void SwitchState(GameState state) {
+        OnEnd_State(currentState);
+        currentState = state;
+        OnStart_State(currentState);
+    }
+
+    private void OnStart_State(GameState state) {
+        switch (state) {
+            case GameState.playMode:
+
+                break;
+
+            case GameState.killMode:
+
+                break;
+        }
+    }
+
+    private void OnEnd_State(GameState state) {
+        switch (state) {
+            case GameState.playMode:
+
+                break;
+
+            case GameState.killMode:
+
+                break;
+        }
+    }
+}
