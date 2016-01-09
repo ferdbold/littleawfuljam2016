802ed4822b6e29a860a5030ad55ec4438504baf8
Antoine Brouillette <antoineqc@hotmail.com>
2016-01-09 14:39:12 -0500

    Conveyor Belt close #7

---
+++ Assets/Scripts/EnviroElement/ConveyorBelt.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorBelt : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+    void OnTriggerStay(Collider other) {
+        Rigidbody body = other.GetComponent<Rigidbody>();
+        if (body != null) {
+            body.transform.position += transform.forward * speed * Time.deltaTime;
+        }
+    }
+}
