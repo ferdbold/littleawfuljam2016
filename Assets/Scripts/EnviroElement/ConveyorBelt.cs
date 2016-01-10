7f7d011c366c507b3865d3071c5b73a7c4248a97
Antoine Brouillette <antoineqc@hotmail.com>
2016-01-09 19:53:58 -0500

    ConveyorFix

--- Assets/Scripts/EnviroElement/ConveyorBelt.cs
+++ Assets/Scripts/EnviroElement/ConveyorBelt.cs
@@ -9,15 +9,29 @@ public class ConveyorBelt : MonoBehaviour {
     [SerializeField]
     private bool isOn;
 
+    //if the conveyor belt is locking other
+    private bool locking = false;
+
     public void TurnOn() { isOn = true; }
     public void TurnOff() { isOn = false; }
 
     void OnTriggerStay(Collider other) {
-        if (isOn) {
+        if (!locking && isOn) {
             Converable body = other.GetComponent<Converable>();
             if (body != null) {
                 body.locked = true;
                 body.transform.position += transform.forward * speed * Time.deltaTime;
+                locking = true;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (isOn) {
+            Converable body = other.GetComponent<Converable>();
+            if (locking && body != null) {
+                body.locked = false;
+                locking = false;
             }
         }
     }
