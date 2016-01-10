cde756c20c981643fd8888831d9c01c015492d9c
Marc-André Fortin-Leclerc <marc1999_8@hotmail.com>
2016-01-09 20:57:10 -0500

    Ajout de l'emitteur de sang et du mode enabler au minigame

---
+++ Assets/Scripts/KillMode/ModeEnabler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModeEnabler : MonoBehaviour {
+
+    public MiniGameController script;
+
+	// Use this for initialization
+	void Start () {
+        script.enabled = false;
+	}
+
+    public void enableScript()
+    {
+        script.enabled = true;
+    }
+
+    public void disableScript()
+    {
+        script.enabled = false;
+    }
+}
