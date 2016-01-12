c8a51d2add5ba388fa2869461d5ceb5881b111fa
Alexis <alex3521@hotmail.fr>
2016-01-12 16:06:16 -0500

    Small tweak to sloth animations when reaching ending

--- Assets/Scripts/Manager/LevelManager.cs
+++ Assets/Scripts/Manager/LevelManager.cs
@@ -49,7 +49,7 @@ public class LevelManager : MonoBehaviour {
             case GameState.killMode:
                 sloth.GetComponent<SlothController>().DeactivateControls();
                 sloth.transform.DOMove(enemyTarget.transform.position + new Vector3(-0.08f,0.1f,-0.54f),2f);
-                sloth.transform.DORotate(enemyTarget.transform.rotation.eulerAngles, 2f);
+                sloth.transform.DORotate((enemyTarget.transform.rotation.eulerAngles)+ new Vector3(0,180,0), 2f);
                 KillMode_StartKilling();
                 break;
 
