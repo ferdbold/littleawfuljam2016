39c463253e6de1525a96ce164483629c316a9d9e
sprawls <alex3521@hotmail.fr>
2016-01-12 19:33:47 -0500

    Integrated transition to kill mode phase

---
+++ Assets/Scripts/Camera/LookAtCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+
+    public GameObject player;
+
+	void Update () {
+        Vector3 lookAtPos = player.transform.position - transform.position + new Vector3(0,0.2f,0);
+        Quaternion newRot = Quaternion.LookRotation(lookAtPos);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRot , 0.1f);
+	}
+}
--- Assets/Scripts/KillMode/ModeEnabler.cs
+++ Assets/Scripts/KillMode/ModeEnabler.cs
@@ -1,26 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class ModeEnabler : MonoBehaviour {
 
     public MiniGameController script;
     public Canvas killCamCanvas;
+    public Animator animator;
+    public Transform killModeCameraAnchor;
 
-	// Use this for initialization
+    //Cameras
+    private Camera _camera;
+    private TopDownCamera _topDownCamera;
+    private LookAtCamera _lookAtCamera;
+
+    void Awake() {
+        _camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        _topDownCamera = _camera.GetComponent<TopDownCamera>();
+        _lookAtCamera = _camera.GetComponent<LookAtCamera>();
+    }
+
 	void Start () {
         script.enabled = false;
         killCamCanvas.enabled = false;
-	}
+        _lookAtCamera.enabled = false;
+    }
 
     public void enableScript()
     {
         script.enabled = true;
         killCamCanvas.enabled = true;
+        animator.SetBool("InKillMode", true);
+        _topDownCamera.enabled = false;
+        _lookAtCamera.enabled = true;
+        StartCoroutine(CameraAnimOnDelay()); 
     }
 
     public void disableScript()
     {
         script.enabled = false;
         killCamCanvas.enabled = false;
+        animator.SetBool("InKillMode", false);
+        _topDownCamera.enabled = true;
+        _lookAtCamera.enabled = false;
+    }
+
+    IEnumerator CameraAnimOnDelay() {
+        yield return new WaitForSeconds(2.5f);
+        _camera.transform.DOMove(killModeCameraAnchor.transform.position, 3.5f);
     }
 }
